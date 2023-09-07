5be316651bfeb23b6e8172078c4fbdcca81f9df9
ipekelaytac <aytacipekel@gmail.com>
2023-09-07 19:25:34 +0300

    fronted layout

--- routes/indexRoutes.js
+++ routes/indexRoutes.js
@@ -1,7 +1,16 @@
 const express = require('express'),
     router = express.Router();
 router.get("/", (req,res)=>{
-    res.send("Test Ediyoruz.")
+    res.render('index')
+})
+router.get("/blog", (req,res)=>{
+    res.render('blog')
+})
+router.get("/hakkimizda", (req,res)=>{
+    res.render('about')
+})
 
+router.get("/iletisim", (req,res)=>{
+    res.render('contact')
 })
 module.exports = router;
